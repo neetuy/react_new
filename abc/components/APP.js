c93a141974fa07a026f96779acdb39e75b5afc6c
neetu <neetuy1455@gmail.com>
2017-03-10 17:36:58 +0530

    initial modified

---
+++ abc/components/APP.js
@@ -0,0 +1,47 @@
+var React = require('react');
+var Router = require('react-router');
+var RouteHandler = Router.RouteHandler;
+
+var io = require('socket.io-client');
+var Header = require('./parts/Header');
+
+var APP = React.createClass({
+
+    getInitialState() {
+        return {
+            status: 'disconnected',
+            title: ''
+        }
+    },
+
+    componentWillMount() {
+        this.socket = io('http://localhost:3000');
+        this.socket.on('connect', this.connect);
+        this.socket.on('disconnect', this.disconnect);
+        this.socket.on('welcome', this.welcome);
+    },
+
+    connect() {
+        this.setState({ status: 'connected' });
+    },
+
+    disconnect() {
+        this.setState({ status: 'disconnected' });
+    },
+
+    welcome(serverState) {
+        this.setState({ title: serverState.title });
+    },
+
+    render() {
+        return (
+            <div>
+                <Header title={this.state.title} status={this.state.status} />
+                <RouteHandler />
+            </div>
+        );
+    }
+
+});
+
+module.exports = APP;
